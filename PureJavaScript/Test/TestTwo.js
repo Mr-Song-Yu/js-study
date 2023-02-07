d37f1d908dc18898b3b84c94254888b5e2d3ce3f
Mr-Song-Yu <48613774+Mr-Song-Yu@users.noreply.github.com>
2023-02-07 22:44:42 +0800

    0207

--- PureJavaScript/Test/TestTwo.js
+++ PureJavaScript/Test/TestTwo.js
@@ -7,3 +7,39 @@ function bar() {
 bar();
 // alert(foo);
 console.log(foo);
+
+async function async1() {
+  console.log(2);
+  await async2();
+  console.log(6);
+}
+
+async function async2() {
+  console.log(3);
+}
+
+console.log(1);
+
+setTimeout(() => {
+  console.log(8);
+}, 0);
+
+async1();
+
+new Promise(function (resolve) {
+  console.log(4);
+  resolve();
+}).then(function () {
+  console.log(7);
+});
+
+console.log(5);
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
